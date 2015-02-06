b05df88d4b7d158a1723942f87b07ad8caba6955
rbyelyy <rbyelly@rbyellys-MacBook-Pro.local>
2015-02-06 11:06:59 +0100

    MyRepo: Added sum of integer challenge

---
+++ codeeval/sum_of_integer_from_file.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+import sys
+
+
+def sum_of_numbers_in_file(path):
+    """
+    Sum of numbers in the file
+    :rtype : object
+    """
+    with open(path, 'r') as infile:
+        return [int(i) for i in infile]
+
+
+if __name__ == "__main__":
+    assert sys.argv[1] is not None, "Please set the path to the file."
+    print sum(sum_of_numbers_in_file(path=sys.argv[1]))
