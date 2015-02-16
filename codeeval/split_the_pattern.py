6d7ff7dbc2a2013731b229a9956dcce695e31ba2
Roman Byelyy <rbyelyy@gmail.com>
2015-02-16 16:54:53 +0100

    Added big date challenge

---
+++ codeeval/split_the_pattern.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+import sys
+
+def read_from_file(path):
+    """
+    Sum of numbers in the file
+    :rtype : object
+    """
+
+    with open(path, 'r') as infile:
+        return [_ for _ in infile]
+
+
+if __name__ == "__main__":
+    file_content = read_from_file(sys.argv[1])
+    for i in file_content:
+        i = i.translate(None, '\n').split(' ')
+        if '+' in i[1]:
+            element_position = i[1].index('+')
+            print int(i[0][element_position:]) + int(i[0][0:element_position])
+        else:
+            element_position = i[1].index('-')
+            print int(i[0][0:element_position]) - int(i[0][element_position:])
+
+
+
