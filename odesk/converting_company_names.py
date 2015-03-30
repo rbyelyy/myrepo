4f3dd5d37352fa534bd79b1abaeaa3c051c41184
Roman Byelyy <rbyelyy@gmail.com>
2015-03-30 14:22:58 +0200

    Updated version

--- odesk/converting_company_names.py
+++ odesk/converting_company_names.py
@@ -7,21 +7,30 @@ import sys
 
 
 def download_nasdaq_company_list(file_name, url):
+    """
+    Download file with up to date NASDAQ company names 
+    """
     response = urllib2.urlopen(url)
     with open(file_name, 'w') as f:
         f.write(response.read())
 
 
 def get_dirty_company_list(file_name):
+    """
+    Read 'dirty' company names from file into list
+    """
     with open(file_name, 'r') as f:
         return f.readlines()
 
 
 def levenshtein(word_one, word_two):
+    """
+    Figure out difference between two sequences. (Based on 'Levenshtein distance')
+    """
     if len(word_one) < len(word_two):
         return levenshtein(word_two, word_one)
 
-    # len(s1) >= len(s2)
+    # len(word_one) >= len(word_one)
     if len(word_two) == 0:
         return len(word_one)
 
@@ -39,7 +48,17 @@ def levenshtein(word_one, word_two):
     return previous_row[-1]
 
 
-def convert_data_to_dict(file_name):
+def hamming_distance(word_one, word_two):
+    """Return the Hamming distance between equal-length sequences"""
+    if len(word_one) != len(word_two):
+        raise ValueError("Undefined for sequences of unequal length")
+    return sum(ch1 != ch2 for ch1, ch2 in zip(word_one, word_two))
+
+
+def convert_nasdaq_data_to_dict(file_name):
+    """
+    Convert company name and ticker from NASDAQ file into dict
+    """
     res = {}
     with open(file_name, 'r') as f:
         for line in f:
@@ -50,37 +69,68 @@ def convert_data_to_dict(file_name):
 
 
 def clean_up_string(str):
+    """
+    Remove all non alphabetic and numeric chars from the string. Make string lowercase.
+    """
     str = str.lower()
     str = re.sub(r'\W+', '', str)
     return str
 
 
 def create_clean_csv(mydict):
+    """
+    Create CSV file with company name and tickers 
+    """
     writer = csv.writer(open('create_clean_csv.csv', 'wb'))
     for key, value in mydict.items():
         writer.writerow([key, value])
 
 
+def delete_tmp_file():
+    """
+    Delete tmp files (NASDAQ)
+    """
+    os.remove('company_name_from_nasdaq')
+
+
 if __name__ == "__main__":
+    nasdaq_url = 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt'
+    nasdaq_file_name = 'company_name_from_nasdaq'
+
+    dirty_file_name = 'company_name_for_converting'
     result = {}
     ticker_and_company = {}
-    download_nasdaq_company_list('company_name_from_nasdaq',
-                                 'ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt')
-    company_list_for_covert = get_dirty_company_list('company_name_for_converting')
 
-    get_company_tickers_from_nasdaq = convert_data_to_dict('company_name_from_nasdaq')
+    # Download and clean up data from NASDAQ
+    download_nasdaq_company_list(nasdaq_file_name, nasdaq_url)
+    get_company_tickers_from_nasdaq = convert_nasdaq_data_to_dict(nasdaq_file_name)
+
+    # Get dirty company names
+    company_list_for_covert = get_dirty_company_list(dirty_file_name)
 
     for dirty_company_name in company_list_for_covert:
         dirty_company_name = clean_up_string(dirty_company_name)
         for key, value in get_company_tickers_from_nasdaq.items():
             value = clean_up_string(value)
-            f = levenshtein(dirty_company_name, value)
-            ticker_and_company[key] = f
+
+            # Hamming algorithm
+            if len(dirty_company_name) == len(value):
+                f = hamming_distance(dirty_company_name, value)
+                if f < 4:
+                    ticker_and_company[key] = f
+                    break
+            else:
+                # Leventstein algorithm
+                f = levenshtein(dirty_company_name, value)
+                ticker_and_company[key] = f
+                if f < 3:
+                    break
+
         min_val = min(ticker_and_company.itervalues())
         ticker = [k for k, v in ticker_and_company.iteritems() if v == min_val]
         result[ticker[0]] = get_company_tickers_from_nasdaq[ticker[0]]
         create_clean_csv(result)
-
+    delete_tmp_file()
 
 
 
