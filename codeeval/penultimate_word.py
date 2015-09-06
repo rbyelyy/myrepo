b2b2d0f98414f22b442545e0278fdc63b7c1587a
Roman Byelyy <rbyelyy@gmail.com>
2015-09-06 21:36:55 +0200

    Create penultimate_word.py

---
+++ codeeval/penultimate_word.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import sys
+
+
+def read_file():
+    with open(sys.argv[1]) as fp:
+        return fp.readlines()
+
+if __name__ == "__main__":
+    lines = filter(None, [i.replace('\n', '') for i in read_file()])
+
+    for i in lines:
+        print i.split(' ')[-2:-1][0]
